60250060a4dc177db86406d2c4dee6dbe7c72518
tyort <neotyo1986@gmail.com>
2022-12-03 20:56:39 +0300

    Больше моковых данных

--- mock/users.js
+++ mock/users.js
@@ -71,4 +71,148 @@ export const users = [
     isChecked: false,
     friendsCount: 3
   },
+  {
+    fullName: 'Kate Popova',
+    birthYear: 2001,
+    profession: 'singer',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 12
+  },
+  {
+    fullName: 'Ree John',
+    birthYear: 1935,
+    profession: 'soldier',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 10
+  },
+  {
+    fullName: 'Tanya Lee',
+    birthYear: 1956,
+    profession: 'dancer',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 2
+  },
+  {
+    fullName: 'Rick Nolan',
+    birthYear: 1883,
+    profession: 'manager',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 38
+  },
+  {
+    fullName: 'Elton John',
+    birthYear: 1924,
+    profession: 'teacher',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 113
+  },
+  {
+    fullName: 'Nina Solf',
+    birthYear: 2003,
+    profession: 'student',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 63
+  },
+  {
+    fullName: 'Jack Jack',
+    birthYear: 1986,
+    profession: 'player',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 34
+  },
+  {
+    fullName: 'Enzo Gorlami',
+    birthYear: 1995,
+    profession: 'director',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 31
+  },
+  {
+    fullName: 'Kila Pony',
+    birthYear: 2007,
+    profession: 'mother',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 3
+  },
+  {
+    fullName: 'Kate Popova',
+    birthYear: 2001,
+    profession: 'singer',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 12
+  },
+  {
+    fullName: 'Ree John',
+    birthYear: 1935,
+    profession: 'soldier',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 10
+  },
+  {
+    fullName: 'Tanya Lee',
+    birthYear: 1956,
+    profession: 'dancer',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 2
+  },
+  {
+    fullName: 'Rick Nolan',
+    birthYear: 1883,
+    profession: 'manager',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 38
+  },
+  {
+    fullName: 'Elton John',
+    birthYear: 1924,
+    profession: 'teacher',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 113
+  },
+  {
+    fullName: 'Nina Solf',
+    birthYear: 2003,
+    profession: 'student',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 63
+  },
+  {
+    fullName: 'Jack Jack',
+    birthYear: 1986,
+    profession: 'player',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 34
+  },
+  {
+    fullName: 'Enzo Gorlami',
+    birthYear: 1995,
+    profession: 'director',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 31
+  },
+  {
+    fullName: 'Kila Pony',
+    birthYear: 2007,
+    profession: 'mother',
+    isVisible: true,
+    isChecked: false,
+    friendsCount: 3
+  },
 ];
